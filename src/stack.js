e707f6183d42b9d4e1ff6f054540bb11563da45d
Alexey Trukhlyayev <exact84@gmail.com>
2025-01-05 23:30:53 +0500

    feat: 1 task done

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  body = [];
+  i = 0;
+  push(element) {
+    this.body[this.i] = element;
+    this.i += 1;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.i -= 1;
+    return this.body[this.i];
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.body[this.i - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
